922684adc59a448eaab4688b30ba8a17dad63b61
maroberto <maroberto13@gmail.com>
2021-10-25 12:10:48 -0300

    Aula 12 JavaScript/ organição

---
+++ JavaScript aulas/aula-12/ex011.js
@@ -0,0 +1,9 @@
+var idade = 17
+
+if (idade < 16){
+  console.log("Não vota!")
+}else if (idade < 18 || idade > 65){
+  console.log("Voto opcinal!")  
+} else{
+  console.log("Voto obrigatorio!")
+}
