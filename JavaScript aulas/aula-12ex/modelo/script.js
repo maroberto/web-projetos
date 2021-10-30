bfbe0e8673a77c487c66ba98c4375c1f47dfc72d
maroberto <maroberto13@gmail.com>
2021-10-30 17:15:27 -0300

    edição javascript parte2

--- JavaScript aulas/aula-12ex/modelo/script.js
+++ JavaScript aulas/aula-12ex/modelo/script.js
@@ -1,1 +1,14 @@
-alert("olá!")
+function verificar(){
+  var data = new Date();
+  var ano = data.getFullYear();
+  var fano = document.getElementById("txtano")
+  var res = document.querySelector("div#res")
+  if (fano.value.length == 0 || fano.value > ano){
+    alert("[ERRO] verifique os dados e tente novamente!")
+  }else{
+    var fsex = document.getElementsByTagName("radsex")
+    var idade = ano - Number(fano.value)
+    res.innerHTML = `idade calculada: ${idade}`
+  }
+}
+// 2247 minutos
